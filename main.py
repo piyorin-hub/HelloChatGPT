e4284bf6ec00c79bd35e4a17e790f7c95f694976
piyorin85 <noripcode@gmail.com>
2023-03-17 00:49:00 +0900

    accept input

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import openai
 
 openai.api_key = "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"
+questions = input()
 
 res = openai.ChatCompletion.create(
     model = "gpt-3.5-turbo",
     messages = [
         {
             "role":"user",
-            'content': "ChatGPTとはなんですか？"
+            'content': questions
         }
     ]
 )    
-print(res) # レスポンス（res）を出力する
 print(res["choices"][0]["message"]["content"]) # レスポンス（res）の中から返答のみを指定して出力する
